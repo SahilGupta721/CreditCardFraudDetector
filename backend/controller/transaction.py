aca242e4368920e58f921964a47069702f594ce0
Sahil Gupta <sahilgupta70500@gmail.com>
2026-01-09 15:38:14 -0500

    backend is complete
    mongodb processed transaction for frontend dashboard and file upload feature are done

--- backend/controller/transaction.py
+++ backend/controller/transaction.py
@@ -1,11 +1,16 @@
 """
-The motive of this code to generate predicted values for some already existed data in MongoDB, so once we got the predicted values that
-output gonna be used for our frontend dashboard
+The motive of this code to generate predicted values from some already existed data in MongoDB, so once we got the predicted values that
+output gonna be used for our frontend dashboard, since predicted values are already stored inside collection so we don't 
+wanna insert same documents again, instead we get all documents to populate frontend
 
 """
 import pandas as pd
 import joblib
 from core.database import database_transaction_pred, database_transaction_raw
+from fastapi import UploadFile
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+from fastapi.responses import StreamingResponse
 
 # Load model & scaler once
 model = joblib.load("ai_model/fraud_model.pkl")
@@ -23,13 +28,41 @@ def make_predictions():
     X_new[['Time','Amount']] = scale.transform(X_new[['Time','Amount']])
 
     y_pred_proba_fraud = model.predict_proba(X_new)[:, 1]
+    #When the proability is more than 0.5 we will flag as fraud in terms of credit card transaction
     y_pred_class = (y_pred_proba_fraud > 0.5).astype(int)
 
     df['PredictedClass'] = y_pred_class
     df['FraudProbability'] = y_pred_proba_fraud
 
-    # Store predictions
+    # Storing predictions 
     #database_transaction_pred.insert_many(df.to_dict(orient="records"))
 
     # Return relevant fields for dashboard
-    return df[["UserID", "Merchant", "Amount", "PredictedClass", "FraudProbability"]].to_dict(orient="records")
+    return df[["UserID", "Merchant", "Amount", "PredictedClass", "FraudProbability"]].to_dict(orient="records") #converts dataframe into JSON format
+
+async def file_pred(file:UploadFile):
+    
+    content=await file.read()
+    df=pd.read_excel(BytesIO(content))
+    features = ['Time'] + [f'V{i}' for i in range(1,29)] + ['Amount']
+    X_new = df[features]
+    X_new[['Time','Amount']] = scale.transform(X_new[['Time','Amount']])
+
+    y_pred_proba_fraud = model.predict_proba(X_new)[:, 1]
+    #When the proability is more than 0.5 we will flag as fraud in terms of credit card transaction
+    y_pred_class = (y_pred_proba_fraud > 0.5).astype(int)
+
+    df['PredictedClass'] = y_pred_class
+    df['FraudProbability'] = y_pred_proba_fraud
+    output = BytesIO()
+    df.to_excel(output, index=False)
+    output.seek(0)  
+    
+    #Sending file to the frontend
+    return StreamingResponse(
+    output,
+    media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    headers={
+        "Content-Disposition": "attachment; filename=predicted_transactions.xlsx"
+    }
+)
