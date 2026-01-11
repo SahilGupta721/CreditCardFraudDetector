07c3cb0ef392a688c2ce6f8c83905c0304caa561
Sahil Gupta <sahilgupta70500@gmail.com>
2026-01-11 01:52:30 -0500

    done

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://credit-card-fraud-detector-five.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
