87770049d275880be2d97620041085fc253d4d9a
Sahil Gupta <sahilgupta70500@gmail.com>
2026-01-11 01:11:52 -0500

    added

--- backend/core/database.py
+++ backend/core/database.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 connection_string = os.getenv("MONGO_URI")
 
-client=MongoClient(connection_string,serverSelectiontimeoutMS=3000)
+client = MongoClient(connection_string, serverSelectionTimeoutMS=3000)
 database=client['creditcard-fraud-detection']
 database_transaction_raw=database['transaction_raw']
 database_transaction_pred=database['transaction_pred']
